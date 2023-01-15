0e1e6f23f154f623ea1d72cf25afbf18de1a8d12
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2023-01-15 21:21:52 +0530

    starting section 7

---
+++ section7/exp_7.1_nXn_MatrixAndSymmetricOrNot.c
@@ -0,0 +1,8 @@
+/*This program takes nXn matrix and checks whether a symmetric matrix or not*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   printf("\n This program takes nXn matrix and checks whether a symmetric matrix or not \n");
+   
+}
